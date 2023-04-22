17d12ef43605965c2fe2c2263d0d92ae929266a4
dweggg <dredondovinolo@gmail.com>
2023-04-22 15:18:11 +0200

    new features in control plot and FOC simulation

--- Control/FOC_DQ0_parameters.m
+++ Control/FOC_DQ0_parameters.m
@@ -3,11 +3,13 @@ clc, clear
 %% Permanent magnet synchronous machine constant parameters
 motor = struct();
 
-motor.lambda = 0.10642288;                    % [Wb] PM flux linkage
-motor.Ld = 0.52e-3;                           % [H] d-axis inductance
-motor.Lq = 1.26e-3;                           % [H] q-axis inductance
-motor.Rs = 115e-3;                            % [Ohm] Stator phase resistance (phase-to-phase/2)
-motor.n = 8;                                  % [ad] Number of poles 
+motor.lambda = 35e-3;                         % [Wb] PM flux linkage
+motor.Ld = 0.2372e-3;                         % [H] d-axis inductance
+motor.Lq = 0.4459e-3;                         % [H] q-axis inductance
+motor.Rs = 0.0201;                            % [Ohm] Stator phase resistance (phase-to-phase/2)
+motor.n = 6;                                  % [ad] Number of poles 
 motor.J = 0.01;                               % [N·m^2] Rotor moment of inertia
 motor.B = 0.005;                              % [ad] Rotor viscous friction coefficient
-motor.maxRPM = 20000;                         % [rpm] Motor maximum angular speed
+motor.maxRPM = 25000;                         % [rpm] Motor maximum angular speed
+motor.nomRPM = 12500;                         % [rpm] Motor maximum angular speed
+motor.I_max = 150;                            % [A] Motor maximum output current
--- Control/control_plot.m
+++ Control/control_plot.m
@@ -1,25 +1,29 @@
 clc, clear
 
 %% Permanent magnet synchronous machine constant parameters
-lambda = 0.06711353021;                 % [Wb] PM flux linkage
-Ld = 0.52e-3;                           % [H] d-axis inductance
-Lq = 1.26e-3;                           % [H] q-axis inductance
+lambda = 35e-3;                         % [Wb] PM flux linkage
+Ld =  0.2372e-3;                        % [H] d-axis inductance
+Lq =  0.4459e-3;                        % [H] q-axis inductance
 epsilon = Lq/Ld;                        % [ad] Saliency ratio
-Rs = 104e-3;                            % [Ohm] Stator phase resistance (phase-to-phase/2)
-n = 8;                                  % [ad] Number of poles 
+Rs = 0.0201;                            % [Ohm] Stator phase resistance (phase-to-phase/2)
+n = 6;                                  % [ad] Number of poles 
 J = 0.01;                               % [N·m^2] Rotor moment of inertia
 B = 0.005;                              % [ad] Rotor viscous friction coefficient
-maxRPM = 20000;                         % [rpm] Motor maximum angular speed
-Is_max = 125;                           % [A] Maximum d-q current (sqrt(i_d^2+i_q^2))
+P_max = 35e3;                           % [W] Maximum output power
+maxRPM = 25000;                         % [rpm] Motor maximum angular speed
 Vb = lambda * maxRPM*(2*pi/60) / (n/2); % [V] Base voltage
-Vbat = 500;                             % [V] Battery DC voltage
+Vbat = 600;                             % [V] Battery DC voltage
 Vs_max = Vbat / (sqrt(3)*Vb);           % [V] Maximum d-q voltage (Maximum Torque per Voltage Flux-Weakening strategy with speed limiter for PMSM drives, 2020)
+Is_max = 0.05 * P_max / Vs_max;         % [A] Maximum d-q current (sqrt(i_d^2+i_q^2))
 
+%% Commands
+Te_command = 8;
+speed_command = 7500*2*pi/60;
 %% dq plot
 idiq = figure;
 
 id_lim = [-200, 50];
-iq_lim = [-150, 150];
+iq_lim = [-200, 200];
 
 axis([id_lim, iq_lim])             % [A] Current maximum values
 xlabel('i_d [A]') 
@@ -37,11 +41,11 @@ ax.YAxisLocation="origin";
 hold on
 
 tic
-Te_vals = [-150, -120, -50, -20, -5, 5, 20, 50, 120, 150]; % [Nm] Torque values
+Te_vals = [-50, -26, -20, -5, -1, 1, 5, 20, 26, 50]; % [Nm] Torque values
 
 %id = id_lim(1):1:id_lim(2);
 %iq = iq_lim(1):1:iq_lim(2);
-
+    
 %tq_color_plot = pcolor(id, iq, abs((3/2)*(n/2)*(lambda.*iq'+(Ld-Lq).*iq'.*id)));
 
 %set(tq_color_plot, 'EdgeColor', 'none');
@@ -50,13 +54,15 @@ Te_vals = [-150, -120, -50, -20, -5, 5, 20, 50, 120, 150]; % [Nm] Torque values
 syms id
 
 for i = 1:length(Te_vals)
-    tq_plot = fplot(sign(Te_vals(i))*sqrt(2*abs(Te_vals(i))/(3*(n/2)*id*lambda*(Ld-Lq))), 'k', 'LineWidth', 1.5); % IPMSM torque equation, solved for iq
-    text(-50, sign(Te_vals(i))*sqrt(2*Te_vals(i)/(3*(n/2)*sign(Te_vals(i))*-50*lambda*(Ld-Lq))), sprintf('%.f N·m',Te_vals(i)),'Color','black','FontSize',14)
+    tq_plot = fplot(4*Te_vals(i)/(3*n*(lambda+(Ld-Lq)*id)), 'k', 'LineWidth', 0.75); % IPMSM torque equation, solved for iq
+    text(-50, 4*Te_vals(i)/(3*n*(lambda+(Ld-Lq)*-50)), sprintf('%.f N·m',Te_vals(i)),'Color','black','FontSize',12)
 end
+
+fplot(4*Te_command/(3*n*(lambda+(Ld-Lq)*id)), 'r', 'LineWidth', 1.5); % IPMSM torque equation, solved for iq
+text(-50, 4*Te_command/(3*n*(lambda+(Ld-Lq)*-50)), sprintf('%.f N·m',Te_command),'Color','red','FontSize',14)
 
 clear id iq
 toc
-
 %% Current limit circle
 
 alpha = linspace(0,2*pi);
@@ -67,7 +73,7 @@ clear alpha
 
 %% MTPA curve
 
-Te_vals = [-150:1:-.1 .1:1:150]; % More torque values, excluding 0
+Te_vals = [-50:.5:-.1 .1:.5:50]; % More torque values, excluding 0
 iq_ref_MTPA = zeros(1,length(Te_vals));
 
 % Iq_ref from torque, MTPA. Extracted from
@@ -86,7 +92,7 @@ for i = 1:length(Te_vals)
 end
 toc
 
-clear iq eqn %solutions real_sols
+clear iq eqn solutions real_sols
 
 % Id_ref from Iq_ref, MTPA. Extracted from
 % https://iris.unipa.it/retrieve/handle/10447/406125/862725/document.pdf,
@@ -94,13 +100,17 @@ clear iq eqn %solutions real_sols
 id_ref_MTPA = -lambda/(2*(Ld-Lq)) - sqrt((lambda/(2*(Ld-Lq)))^2+iq_ref_MTPA.^2);
 
 for i = 1:length(Te_vals)
-    MTPA_plot = plot(id_ref_MTPA(i), iq_ref_MTPA(i), 'xb', 'LineWidth',3);
+    if abs(Te_vals(i)) <= Te_command*sign(Te_vals(i))
+       MTPA_plot = plot(id_ref_MTPA(i), iq_ref_MTPA(i), '+r', 'LineWidth',2);
+    else
+        MTPA_plot = plot(id_ref_MTPA(i), iq_ref_MTPA(i), '.b', 'LineWidth',1);
+    end
 
 end
 
 %% Voltage ellipse
 
-speed_vals = [50, 62.5, 75, 100, 150, 200, 500, 800];
+speed_vals = [linspace(0, maxRPM*2*pi/60, 6) speed_command];
 
 % Voltage ellipse eqn
 % (id-lambda/Ld)^2/(Vs_max/(Ld*speed))^2+iq^2/(Vs_max/(Lq*speed))^2<=1
@@ -114,13 +124,18 @@ for i = 1:length(speed_vals)
     
     id=h_ellipse+a_ellipse.*cos(t);
     iq=b_ellipse.*sin(t);
-    
-    voltageEllipse_plot = plot(id, iq, 'm','LineWidth',1.5);
+    if i == length(speed_vals)
+        plot(id, iq, '--r','LineWidth',2);
+        text(h_ellipse, min(iq), sprintf('%.f rpm',speed_vals(i)*60/(2*pi)),'Color','red','FontSize',14);
 
-    text(h_ellipse, min(iq), sprintf('%.f rad/s',speed_vals(i)),'Color','magenta','FontSize',14);
+    else
+        voltageEllipse_plot = plot(id, iq, '--k','LineWidth',1);
+        text(h_ellipse, min(iq), sprintf('%.f rpm',speed_vals(i)*60/(2*pi)),'Color','black','FontSize',12);
+
+    end
 end
 
 %% Plot legend
 
 legend([i_lim_plot(1), tq_plot(1), MTPA_plot(1), voltageEllipse_plot(1)], 'Current limit [A]', 'Torque curves', 'MTPA hyperbola', 'Voltage limit ellipses')
-% TODO: Voltage ellipse, speed limit, FW, MTPV
+% TODO: speed limit, FW, MTPV
