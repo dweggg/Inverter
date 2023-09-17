0f58a409fe98c8bcb44d3f59e6c7e23657554dcd
dweggg <dredondovinolo@gmail.com>
2023-09-17 18:58:40 +0200

    MTPA & CLC, EMR library

--- Control/FOC_DQ0_parameters.m
+++ Control/FOC_DQ0_parameters.m
@@ -2,20 +2,51 @@ clc, clear
 
 %% Permanent magnet synchronous machine constant parameters
 motor = struct();
+motor.n = 10;                                % [ad] Number of poles 
+motor.kE = 18.8;                             % [Vrmsphn/krpm(wm)] Speed constant 
+motor.lambda = motor.kE*(60/(2*pi))/(1000*(motor.n/2));  % [Wb] PM flux linkage
+motor.Ld =  0.12e-3;                         % [H] d-axis inductance
+motor.Lq =  0.24e-3;                         % [H] q-axis inductance
+motor.epsilon = motor.Lq/motor.Ld;           % [ad] Saliency ratio
+motor.Rs = 0.135;                            % [Ohm] Stator phase resistance (phase-to-phase/2)
+motor.maxRPM = 20000;                        % [rpm] Motor maximum angular speed
+motor.K_FW = 0.8;                            % [%] Field Weakening safety factor
+motor.Te_max = 30;                           % [N·m] Motor maximum angular torque
+motor.I_max = 105;                           % [A] Maximum d-q current (sqrt(i_d^2+i_q^2))
+        
+%% Inverter parameters
+inv = struct();
 
-motor.Ke = 0.1;                                     % [V/(rad/s)] Speed constant
-motor.n = 6;                                        % [ad] Number of poles 
-motor.lambda = sqrt (2/3) * motor.Ke / (motor.n/2); % [Wb] PM flux linkage
-motor.Ld = 0.2372e-3;                               % [H] d-axis inductance
-motor.Lq = 0.4459e-3;                               % [H] q-axis inductance
-motor.Rs = 0.0201;                                  % [Ohm] Stator phase resistance (phase-to-phase/2)
-motor.J = 0.01;                                     % [N·m^2] Rotor moment of inertia
-motor.B = 0.005;                                    % [ad] Rotor viscous friction coefficient
-motor.maxRPM = 20000;                               % [rpm] Motor maximum angular speed
-motor.nomRPM = 12500;                               % [rpm] Motor maximum angular speed
-motor.I_max = 200;                                  % [A] Motor maximum output current
+inv.tr_id = 10e-3;                   % Rise time [s]
+inv.alpha_id = log(9)/inv.tr_id;
+inv.Kp_id = inv.alpha_id*motor.Ld;
+inv.Ki_id = inv.alpha_id*motor.Rs;
+
+inv.tr_iq = 10e-3;                   % Rise time [s]
+inv.alpha_iq = log(9)/inv.tr_iq;
+inv.Kp_iq = inv.alpha_iq*motor.Lq;
+inv.Ki_iq = inv.alpha_iq*motor.Rs;
+
 
 
 %% Battery parameters
 battery = struct();
-battery.V = 580;                                    % [V] Battery voltage
+battery.ns = 140;
+battery.np = 5;
+battery.SoC_init = 0.9;
+battery.SoC = [0 35 40 50 65 85 100];                % [%] Battery SOC LUT
+battery.OCV = [2.5 3 3.2 3.6 3.8 4.0 4.2].*battery.ns;% [V] Battery voltage LUT
+battery.Q = 2.5*battery.np;                          % [A·h] Battery capacity
+battery.R0 = 0.0012;                                 % [V] Battery internal resistance
+battery.R1 = 0.0004;                                 % [V] Battery polarization resistance
+battery.C1 = 4074;                                   % [V] Battery polarization capacitance                                    
+%% Car parameters
+car = struct();
+car.GR = 11.2;                                        % [ad] Gear ratio
+car.Mass = 250;                                     % [kg] Car mass
+car.Rw = 0.228;                                     % [m] Wheel radius
+car.Af = 1.1;                                       % [m^2] Frontal area
+car.Cd = 1.5;                                       % [ad] Drag coefficient
+car.RR = 0.017;                                     % [ad] Rolling resistance coefficient
+
+car.motor_number = 2;                               % [ad] Number of motors in the car
--- Control/control_plot.m
+++ Control/control_plot.m
@@ -1,33 +1,94 @@
 clc, clear
 
 %% Permanent magnet synchronous machine constant parameters
-Ke = 0.1;                               % [V/(rad/s)] Speed constant
-n = 6;                                  % [ad] Number of poles 
-%n = 8; Motor 2017-2023
-lambda = sqrt(2/3) * Ke / (n/2);        % [Wb] PM flux linkage
-%lambda = 0.10642288363; Motor 2017-2023
-Ld =  0.1269e-3;                        % [H] d-axis inductance
-%Ld = 520e-6; Motor 2017-2023
-Lq =  0.2268e-3;                        % [H] q-axis inductance
-%Lq = 1.26e-3; Motor 2017-2023
-epsilon = Lq/Ld;                        % [ad] Saliency ratio
-Rs = 0.0201;                            % [Ohm] Stator phase resistance (phase-to-phase/2)
-J = 0.001;                              % [N·m^2] Rotor moment of inertia
-B = 0.001;                              % [ad] Rotor viscous friction coefficient
-P_max = 35e3;                           % [W] Maximum output power
-maxRPM = 25000;                         % [rpm] Motor maximum angular speed
-%maxRPM = 5000; Motor 2017-2023
-Te_max = 30;                            % [N·m] Maximum motor torque
-%Te_max = 150; Motor 2017-2023
-Vbat = 580;                             % [V] Battery DC voltage
-Vs_max = Vbat / sqrt(3);                % [V] Maximum d-q voltage (Maximum Torque per Voltage Flux-Weakening strategy with speed limiter for PMSM drives, 2020)
-Is_max = 200;                           % [A] Maximum d-q current (sqrt(i_d^2+i_q^2))
+% motor = 'e-Tech 2023';
+% motor = 'e-Tech 2017';
+% motor = 'Silence';
+% motor = 'Caruso 2019';
+motor = 'AMK';
 
+switch motor
+    case 'e-Tech 2023'
+        Ke = 0.13;                              % [V/(rad/s)] Mechanical speed constant
+        n = 6;                                  % [ad] Number of poles 
+        lambda_Ke = Ke / sqrt(3);               % [Wb] PM flux linkage
+        lambda = 28.95e-3;                      % [Wb] PM flux linkage
+        Ld =  0.1269e-3;                        % [H] d-axis inductance
+        Lq =  0.2268e-3;                        % [H] q-axis inductance
+        epsilon = Lq/Ld;                        % [ad] Saliency ratio
+        Rs = 0.0201;                            % [Ohm] Stator phase resistance (phase-to-phase/2)
+        P_max = 35e3;                           % [W] Maximum output power
+        SpeedMax = 20000;                       % [rpm] Motor maximum angular speed
+        Te_max = 26;                            % [N·m] Motor maximum angular torque
+        Vbat = 580;                             % [V] Battery DC voltage
+        Vs_max = Vbat / sqrt(3);                % [V] Maximum d-q voltage (Maximum Torque per Voltage Flux-Weakening strategy with speed limiter for PMSM drives, 2020)
+        Is_max = 185;                           % [A] Maximum d-q current (sqrt(i_d^2+i_q^2))
+        Te_command = 26;
+        SpeedRef = 20000*2*pi/60;  
+    case 'e-Tech 2017'
+        Ke = 49.7e-3*60/(2*pi);                 % [Vrms,phph/(rad/s)] Speed constant, Vrms,phph/wm
+        n = 8;                                  % [ad] Number of poles 
+        lambda = Ke / (sqrt(3) * (n/2));        % [Wb] PM flux linkage, Vrms,phn/we
+        Ld =  0.520e-3;                         % [H] d-axis inductance
+        Lq =  1.265e-3;                         % [H] q-axis inductance
+        epsilon = Lq/Ld;                        % [ad] Saliency ratio
+        Rs = 0.104/2;                           % [Ohm] Stator phase resistance (phase-to-phase/2)
+        P_max = 60e3;                           % [W] Maximum output power
+        SpeedMax = 6000;                        % [rpm] Motor maximum angular speed
+        Te_max = 150;                           % [N·m] Motor maximum angular torque
+        Vbat = 580;                             % [V] Battery DC voltage
+        Vs_max = Vbat / sqrt(3);                % [V] Maximum d-q voltage (Maximum Torque per Voltage Flux-Weakening strategy with speed limiter for PMSM drives, 2020)
+        Is_max = 200;                           % [A] Maximum d-q current (sqrt(i_d^2+i_q^2))
+        Te_command = 80;
+        SpeedRef = 7500*2*pi/60;  
+    case 'Silence'
+        n = 40;                                 % [ad] Number of poles 
+        lambda = 0.02282824;                    % [Wb] PM flux linkage
+        Ld =  70e-6;                            % [H] d-axis inductance
+        Lq =  79e-6;                            % [H] q-axis inductance
+        epsilon = Lq/Ld;                        % [ad] Saliency ratio
+        Rs = 0.017;                             % [Ohm] Stator phase resistance (phase-to-phase/2)
+        SpeedMax = 1000;                        % [rpm] Motor maximum angular speed
+        Te_max = 80;                            % [N·m] Motor maximum angular torque
+        Vbat = 48;                              % [V] Battery DC voltage
+        Vs_max = Vbat / sqrt(3);                % [V] Maximum d-q voltage (Maximum Torque per Voltage Flux-Weakening strategy with speed limiter for PMSM drives, 2020)
+        Is_max = 156;                           % [A] Maximum d-q current (sqrt(i_d^2+i_q^2))      
+        Te_command = 66;
+        SpeedRef = 500*2*pi/60;
+    case 'Caruso 2019'
+        n = 6;                                 % [ad] Number of poles 
+        lambda = 0.084;                        % [Wb] PM flux linkage
+        Ld =  9.77E-3;                         % [H] d-axis inductance
+        Lq =  14.94E-3;                        % [H] q-axis inductance
+        epsilon = Lq/Ld;                       % [ad] Saliency ratio
+        Rs = 2.21;                             % [Ohm] Stator phase resistance (phase-to-phase/2)
+        SpeedMax = 4300;                       % [rpm] Motor maximum angular speed
+        Te_max = 2;                            % [N·m] Motor maximum angular torque
+        Vbat = 310;                            % [V] Battery DC voltage
+        Vs_max = Vbat / sqrt(3);               % [V] Maximum d-q voltage (Maximum Torque per Voltage Flux-Weakening strategy with speed limiter for PMSM drives, 2020)
+        Is_max = 3.6*2;                        % [A] Maximum d-q current (sqrt(i_d^2+i_q^2))
+        Is_max = 8.5;
+        Te_command = 1.8;
+        SpeedRef = 380;
 
+    case 'AMK'
+        n = 10;                                % [ad] Number of poles 
+        kE = 18.8;                             % [Vrmsphn/krpm(wm)] Speed constant 
+        lambda = kE*(60/(2*pi))/(1000*(n/2));  % [Wb] PM flux linkage
+        Ld =  0.12e-3;                         % [H] d-axis inductance
+        Lq =  0.24e-3;                         % [H] q-axis inductance
+        epsilon = Lq/Ld;                       % [ad] Saliency ratio
+        Rs = 0.135;                            % [Ohm] Stator phase resistance (phase-to-phase/2)
+        SpeedMax = 20000;                      % [rpm] Motor maximum angular speed
+        Te_max = 30;                           % [N·m] Motor maximum angular torque
+        Vbat = 560;                            % [V] Battery DC voltage
+        Vs_max = Vbat / sqrt(3);               % [V] Maximum d-q voltage (Maximum Torque per Voltage Flux-Weakening strategy with speed limiter for PMSM drives, 2020)
+        Is_max = 105;                          % [A] Maximum d-q current (sqrt(i_d^2+i_q^2))
+        Te_command = 21;
+        SpeedRef = 25000*2*pi/60;
+    otherwise
+end
 
-%% Commands
-Te_command = 10;
-speed_command = 12500*2*pi/60;
 
 %% dq plot
 idiq = figure;
@@ -46,13 +107,13 @@ ax.GridAlpha = 0.5;
 ax.XAxisLocation="origin";
 ax.YAxisLocation="origin";
 
-%% Torque curves & heatmap
+%% Torque curves
 
 hold on
 
 tic
-Te_vals = linspace(-Te_max, Te_max, 12); % [Nm] Torque values
-
+Te_vals = linspace(-Te_max, Te_max, 7); % [Nm] Torque values
+Te_vals = Te_max;
 syms id
 
 for i = 1:length(Te_vals)
@@ -75,7 +136,7 @@ clear alpha
 
 %% MTPA curve
 
-Te_vals = linspace(-Te_max, Te_max, 100); % [Nm] 100 Torque values
+Te_vals = linspace(-Te_max*2, Te_max*2, 100); % [Nm] 100 Torque values;
 iq_ref_MTPA = zeros(1,length(Te_vals));
 
 % Iq_ref from torque, MTPA. Extracted from
@@ -97,6 +158,7 @@ toc
 % Id_ref from Iq_ref, MTPA. Extracted from
 % https://iris.unipa.it/retrieve/handle/10447/406125/862725/document.pdf,
 % eqn 10
+
 id_ref_MTPA = -lambda/(2*(Ld-Lq)) - sqrt((lambda/(2*(Ld-Lq)))^2+iq_ref_MTPA.^2);
 
 for i = 1:length(Te_vals)
@@ -110,17 +172,18 @@ end
 
 %% Voltage ellipse
 I_ck = lambda/Ld;
-speed_vals = [linspace(0.001, maxRPM*2*pi/60, 6), speed_command]; % 10 voltage ellipses
-
+h_ellipse = -I_ck;
+speed_vals = [linspace(0.001, SpeedMax*2*pi/60, 6), SpeedRef]; % speed values
 % Voltage ellipse eqn
-% (id-lambda/Ld)^2/(Vs_max/(Ld*speed))^2+iq^2/(Vs_max/(Lq*speed))^2<=1
+% (id+lambda/Ld)^2/(Vs_max/(Ld*speed))^2+iq^2/(Vs_max/(Lq*speed))^2<=1
+
 for i = 1:length(speed_vals)
 
     h_ellipse = -I_ck;
     a_ellipse = Vs_max./(Ld.*(n/2)*speed_vals(i));
     b_ellipse = Vs_max./(Lq.*(n/2)*speed_vals(i));
     
-    t=-pi:0.01:pi;
+    t=0:0.01:2*pi;
     
     id=h_ellipse+a_ellipse.*cos(t);
     iq=b_ellipse.*sin(t);
@@ -134,6 +197,7 @@ for i = 1:length(speed_vals)
 
     end
 end
+
 %% Plot legend
 
 legend([i_lim_plot(1), tq_plot(1), MTPA_plot(1), voltageEllipse_plot(1)], 'Current limit [A]', 'Torque curves', 'MTPA hyperbola', 'Voltage limit ellipses')
