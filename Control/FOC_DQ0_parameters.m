397e08b147ba9551fce799171cb1aa77ac556de4
dweggg <dredondovinolo@gmail.com>
2023-09-30 20:18:07 +0200

    Torque profile simulation
    Looks very good!

--- Control/FOC_DQ0_parameters.m
+++ Control/FOC_DQ0_parameters.m
@@ -25,7 +25,7 @@ motor.Lq =  0.3674e-3;                       % [H] q-axis inductance
 motor.epsilon = motor.Lq/motor.Ld;           % [ad] Saliency ratio
 motor.Rs = 0.15;                             % [Ohm]     Stator phase resistance (phase-to-phase/2)
 motor.maxRPM = 20000;                        % [rpm] Motor maximum angular speed
-motor.K_FW = 0.80;                           % [%] Field Weakening safety factor
+motor.K_FW = 0.70;                           % [%] Field Weakening safety factor
 motor.Te_max = 26;                           % [N·m] Motor maximum angular torque
 motor.I_max = 103;                           % [A] Maximum d-q current (sqrt(i_d^2+i_q^2))
 %}
@@ -55,16 +55,16 @@ inv.tr_id = inv.Ts_PI*2;                               % Rise time [s]
 inv.alpha_id = log(9)/inv.tr_id;
 inv.Kp_id = inv.alpha_id*motor.Ld;
 inv.Ki_id = inv.alpha_id*motor.Rs;
-inv.Kaw_id = 1/inv.Ki_id;                              % Not actually sure of this, but works fine
+inv.Kaw_id = 2/inv.Ki_id;                              % Not actually sure of this, but works fine
 
 inv.tr_iq = inv.Ts_PI*2;                               % Rise time [s]
 inv.alpha_iq = log(9)/inv.tr_iq;
 inv.Kp_iq = inv.alpha_iq*motor.Lq;
 inv.Ki_iq = inv.alpha_iq*motor.Rs;
-inv.Kaw_iq = 1/inv.Ki_iq;                              % Not actually sure of this, but works fine
+inv.Kaw_iq = 2/inv.Ki_iq;                              % Not actually sure of this, but works fine
 
 inv.FW.KFW = motor.K_FW;                               % Relationship between the maximum available voltage and the applicable voltage
-inv.FW.tsV = 100e-3;                                   % [s] Flux weakening closed-loop settling time 
+inv.FW.tsV = 500e-3;                                   % [s] Flux weakening closed-loop settling time 
 inv.FW.Is_WP = motor.I_max;                            % [A] Current magnitude working point
 inv.FW.gamma_WP = 3*pi/4;                              % [rad] Current angle working point
 
@@ -79,8 +79,8 @@ inv.FW.A = 2*inv.FW.Is_WP*(motor.Rs*inv.FW.we_WP*inv.FW.tsV*inv.FW.Is_WP*(motor.
 inv.FW.B = inv.FW.Is_WP*cos(inv.FW.gamma_WP)*(5*(motor.Ld-motor.Lq)*(motor.Rs-inv.FW.tsV*(motor.Ld+motor.Lq)*inv.FW.we_WP^2/5)*inv.FW.Is_WP*sin(inv.FW.gamma_WP) + motor.lambda*inv.FW.we_WP*(motor.Rs*inv.FW.tsV - 5*motor.Lq));
 inv.FW.C = inv.FW.Is_WP*inv.FW.we_WP*(sin(inv.FW.gamma_WP)*motor.Ld*inv.FW.we_WP*inv.FW.tsV*motor.lambda + inv.FW.Is_WP*(motor.Rs*(motor.Ld-motor.Lq)*inv.FW.tsV+5*motor.Ld*motor.Lq));
 
-inv.FW.Kp_V_Delta = 0;
 inv.FW.Ki_V_Delta = -5*(battery.OCV(end)/sqrt(3))/(inv.FW.A + inv.FW.B - inv.FW.C);
+inv.FW.Kp_V_Delta = inv.FW.Ki_V_Delta/10000;
 
 %inv.FW.K0_V_Delta = inv.FW.Kp_V_Delta + inv.FW.Ki_V_Delta*inv.Ts_PI/2;
 %inv.FW.K1_V_Delta = -inv.FW.Kp_V_Delta + inv.FW.Ki_V_Delta*inv.Ts_PI/2;
@@ -96,4 +96,8 @@ car.Af = 1.1;                                       % [m^2] Frontal area
 car.Cd = 1.5;                                       % [ad] Drag coefficient
 car.RR = 0.017;                                     % [ad] Rolling resistance coefficient
 car.T_Te = 0.2;                                     % [s] Time from Te = 0 to Te = Te_max
-car.motor_number = 1;                               % [ad] Number of motors in the car
+car.motor_number = 2;                               % [ad] Number of motors in the car
+
+%% Torque load profile
+
+load("TemRef_pu_endurance.mat")
