8c3b1fd83678d43063642a42b2b1ea684433c2a3
David Redondo <david.redondo.vinolo@upc.edu>
2024-05-27 11:14:13 +0200

    implemented derating (not tested)

--- SW/Inverter/Core/Src/TASKS_1ms.c
+++ SW/Inverter/Core/Src/TASKS_1ms.c
@@ -45,20 +45,24 @@ void tasks_1ms(void) {
     handle_LED(&led_right, ms_counter);
     handle_LED(&ledError, ms_counter);
 
-    // Read direction switch
+    // Read direction switch GPIO
     handle_direction(&inverter_left.direction, &inverter_right.direction);
 
-
+    // Get enableSW via CAN and enableHW from shutdown circuit GPIO reading
     enable_inverters(inverter_left.enableSW, inverter_right.enableSW, &inverter_left.enable, &inverter_right.enable);
 
-
+    // Acquire temperatures
     inverter_left.tempInverter = get_temperature(rawADC_temp[0], tempInverterLUT);
     inverter_right.tempInverter = get_temperature(rawADC_temp[1], tempInverterLUT);
     inverter_left.tempMotor = get_temperature(rawADC_temp[2], tempMotorLUT);
     inverter_right.tempMotor = get_temperature(rawADC_temp[3], tempMotorLUT);
 
-    // Reference generation
+    // Torque reference handling, without ramp
     inverter_left.reference.torqueRef = handle_torqueRef(torqueRefIn_left, inverter_left.direction, inverter_left.motor->torqueMax, inverter_left.motor->speedMax_RPM, inverter_left.feedback.speedMeas, &inverter_left.speedLoop);
     inverter_right.reference.torqueRef = handle_torqueRef(torqueRefIn_right, inverter_right.direction, inverter_right.motor->torqueMax, inverter_right.motor->speedMax_RPM, inverter_right.feedback.speedMeas, &inverter_right.speedLoop);
 
+
+    // Current reference derating
+    inverter_left.reference.isMaxRef = derate_current_reference(inverter_left.tempMotor, inverter_left.tempInverter, inverter_left.motor->iMax);
+    inverter_right.reference.isMaxRef = derate_current_reference(inverter_right.tempMotor, inverter_right.tempInverter, inverter_right.motor->iMax);
 }
